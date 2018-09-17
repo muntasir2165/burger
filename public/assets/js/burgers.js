ec287d84b379ffa021ebd0437e490e11accc9756
muntasir2165 <muntasir2165@hotmail.com>
2018-09-17 00:49:10 -0400

    added tooltips for buttons

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -3,6 +3,9 @@ $(document).ready(function() {
     newBurgerToEat();
     devourBurger();
     deleteBurger();
+    // shows the tooltip when the cursor hovers over the burger devouring/undo
+    // and delete buttons
+    $('[data-toggle="tooltip"]').tooltip(); 
 });
 
 function newBurgerToEat() {
@@ -89,63 +92,3 @@ function deleteBurgerInDb(id, devoured) {
 }
 
 
-
-// $(function() {
-//   $(".change-sleep").on("click", function(event) {
-//     var id = $(this).data("id");
-//     var newSleep = $(this).data("newsleep");
-
-//     var newSleepState = {
-//       sleepy: newSleep
-//     };
-
-//     // Send the PUT request.
-//     $.ajax("/api/cats/" + id, {
-//       type: "PUT",
-//       data: newSleepState
-//     }).then(
-//       function() {
-//         console.log("changed sleep to", newSleep);
-//         // Reload the page to get the updated list
-//         location.reload();
-//       }
-//     );
-//   });
-
-//   $(".create-form").on("submit", function(event) {
-//     // Make sure to preventDefault on a submit event.
-//     event.preventDefault();
-
-//     var newCat = {
-//       name: $("#ca").val().trim(),
-//       sleepy: $("[name=sleepy]:checked").val().trim()
-//     };
-
-//     // Send the POST request.
-//     $.ajax("/api/cats", {
-//       type: "POST",
-//       data: newCat
-//     }).then(
-//       function() {
-//         console.log("created new cat");
-//         // Reload the page to get the updated list
-//         location.reload();
-//       }
-//     );
-//   });
-
-//   $(".delete-cat").on("click", function(event) {
-//     var id = $(this).data("id");
-
-//     // Send the DELETE request.
-//     $.ajax("/api/cats/" + id, {
-//       type: "DELETE"
-//     }).then(
-//       function() {
-//         console.log("deleted cat", id);
-//         // Reload the page to get the updated list
-//         location.reload();
-//       }
-//     );
-//   });
-// });
